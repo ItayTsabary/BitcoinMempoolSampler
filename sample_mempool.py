6230c3be25ce33e913b8c42f927c1785419705be
Kevin Davis <kjydavis3@gmail.com>
2018-11-20 12:48:33 -0500

    Print the full stack trace of exceptions

--- sample_mempool.py
+++ sample_mempool.py
@@ -5,15 +5,16 @@ Created on 19 July 2017
 '''
 
 import platform
-import time 
+import time
 import os
 import errno
-from bitcoinrpc import connection 
+from bitcoinrpc import connection
 import operator
 from bitcoinrpc.exceptions import _wrap_exception
 import tarfile
 import os.path
 import shutil
+import traceback
 import configparser
 from random import randint
 
@@ -24,7 +25,7 @@ sleepInterval = 10
 
 
 def get_dir_path():
-  return data_dir_path  
+  return data_dir_path
 
 
 def get_sampling_properties():
@@ -34,13 +35,13 @@ def get_sampling_properties():
 def make_tarfile(output_filename, source_dir):
   with tarfile.open(output_filename, "w:gz") as tar:
     tar.add(source_dir, arcname=os.path.basename(source_dir))
-    
+
 def compress_file(file_path):
   #filename, _ = os.path.splitext(file_path)
   filename = file_path
   output_filename = filename + ".tar.gz"
   make_tarfile(output_filename,file_path)
-  
+
 
 def compress_and_delete_file(output_folder_name,output_file_name):
   file_path = os.path.join(output_folder_name,output_file_name)
@@ -56,10 +57,10 @@ def connect_to_node(config):
   hostip = config["server"]["hostip"]
   portnum = config["server"]["portnum"]
 
-  bitcoinCoreConnection = connection.BitcoinConnection(username, 
+  bitcoinCoreConnection = connection.BitcoinConnection(username,
                                                 password,
-                                                host=hostip, 
-                                                port=portnum) 
+                                                host=hostip,
+                                                port=portnum)
   return bitcoinCoreConnection
 
 def create_out_dir(dir_path):
@@ -73,19 +74,19 @@ def round_down(num, divisor):
   return num - (num%divisor)
 
 def output_folder_and_file_names(current_time_sample,block_count):
-  output_file_name = str(block_count) + "_" + current_time_sample + ".log"      
+  output_file_name = str(block_count) + "_" + current_time_sample + ".log"
   rounded_block_number = round_down(block_count,100)
   dir_path = os.path.join(get_dir_path(),str(rounded_block_number))
   create_out_dir(dir_path)
   return dir_path,output_file_name
-              
+
 
 def write_output_file(output_folder_name, output_file_name, mempool):
   file_path = os.path.join(output_folder_name,output_file_name)
   f = open(file_path, 'wb')
   f.write(str(mempool))
   f.close()
-  print os.path.abspath(file_path) , " created " 
+  print os.path.abspath(file_path) , " created "
 
 
 
@@ -98,7 +99,7 @@ def  create_file(current_time_sample,block_count,mempool):
 
   #write file
   write_output_file(output_folder_name, output_file_name, mempool)
-      
+
   #tar gz and delete file
   compress_and_delete_file(output_folder_name,output_file_name)
 
@@ -112,12 +113,12 @@ def main(config):
   bitcoinCoreConnection = connect_to_node(config)
   sleepInterval = get_sampling_properties()
 
-  
+
   # these two dicts keep track of what happened in the previous iteration
 
 
 
-  
+
   while True:
       #print "start of new outer iteration"
 
@@ -126,12 +127,13 @@ def main(config):
       try:
           block_count = bitcoinCoreConnection.getblockcount()
           mempool = bitcoinCoreConnection.getrawmempool(verbose=True)
-          create_file(current_time_sample,block_count,mempool)          
-      except _wrap_exception:
-          print "caught exception " +  str(_wrap_exception) + " at time " + time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-      except Exception:
-          print "caught exception " +  str(Exception) + " at time " + time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-     
+          create_file(current_time_sample,block_count,mempool)
+      except _wrap_exception as e:
+          print "caught exception {} at time {}".format(traceback.print_exc(e), time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()))
+      except Exception as e:
+          print "caught exception {} at time {}".format(traceback.print_exc(e), time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()))
+
+
       time.sleep(sleepInterval)
       #end of "while true"
 
@@ -144,5 +146,5 @@ if __name__== "__main__":
 
   main(config)
 
-    
+
 
